e6b2f2e25da4eed216336f177a1ba0d172189945
ap-justin <justin@angelprotocol.io>
2024-04-04 12:32:54 +0800

    text base

--- src/components/donation/Steps/Splits/Splits.tsx
+++ src/components/donation/Steps/Splits/Splits.tsx
@@ -59,8 +59,8 @@ export default function Split({
   return (
     <div className="grid content-start p-4 @md/steps:p-8">
       <BackBtn type="button" onClick={() => dispatch(setStep("donate-form"))} />
-      <h4 className="mt-4">Sustainability Fund Donation</h4>
-      <p className="mb-6 text-navy-l1">
+      <h4 className="mt-4 text-lg">Sustainability Fund Donation</h4>
+      <p className="mb-8 text-navy-l1">
         Create a sustainable impact: Give today, give forever
       </p>
 
@@ -92,7 +92,7 @@ export default function Split({
         </dl>
       </div>
 
-      <p className="text-sm text-navy-l1 mt-6">
+      <p className="text-navy-l1 mt-6">
         With splitting your donation into sustainable funds, you align
         investments with personal values, improving long-term financial
         performance, reducing risk exposure, and contributing to global
